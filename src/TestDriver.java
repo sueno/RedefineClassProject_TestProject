fa8efe7b623b4a962a6c6759cba09290a6ecdae4
sueno <ueno.ntnu@gmail.com>
2013-01-07 19:34:37 +0900

    add TestDriver.java

---
+++ src/TestDriver.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestDriver {
+
+	static {
+		Weave.redefine("Stub");
+	}
+	
+	@Test
+	public void test_default() {
+		Stub fc = new Stub();
+		assertEquals(1,fc.hoge());
+	}
+	
+	@Test
+	public void test_changeReturn() {
+		Stub fc = new Stub();
+		Weave.defineStub("hoge", "return -1;");
+		assertEquals(-1,fc.hoge());
+
+	}
+	
+	@Test
+	public void test_addThrow() {
+		try {
+			Stub fc = new Stub();
+			Weave.defineStub("hoge", "throw new NullPointerException();");
+			fail("expected:<NullPointerException> but was <"+fc.hoge()+">");
+		} catch (NullPointerException expected) {
+		} catch (Throwable th) {
+			fail("expected:<NullPointerException> but was <"+th.getClass().getName()+"");
+		}
+	}
+
+}
